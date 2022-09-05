9db1035052381b4d290051433a6ffbcda8a6b7fe
Raghuveer-07 <110120267+Raghuveer-07@users.noreply.github.com>
2022-09-06 01:59:30 +0530

    code updated

--- app.js
+++ app.js
@@ -5,20 +5,17 @@ const checkButton = document.querySelector("#check-change");
 const outputBox = document.querySelector("#output-box");
 
 
-function showMessage(msg,status){
-    if (status==="good"){
+function showMessage(msg, status) {
+    if (status === "good") {
         outputBox.style.color = 'green';
         outputBox.innerText = msg;
 
-    }
-
-    else if (status==="bad"){
+    } else if (status === "bad") {
         outputBox.style.color = 'red';
         outputBox.innerText = msg;
 
-    }
-    else{
-        
+    } else {
+        outputBox.style.color = "black";
         outputBox.innerText = msg;
 
     }
@@ -27,33 +24,33 @@ function showMessage(msg,status){
 
 function profitAndloss(ip, cp, q) {
     if (ip < cp) {
-        var flag="good";
+        var flag = "good";
         var profit = (cp - ip) * q;
         var profitDecimal = profit.toFixed(2);
         var profitPercentage = ((cp - ip) / ip) * 100;
-        var message = "heyy the profit is " + profitDecimal + " and the profit percentage is " + profitPercentage.toFixed(2) + "%" ;
+        var message = "heyy the profit is " + profitDecimal + " and the profit percentage is " + profitPercentage.toFixed(2) + "%";
 
-        
-        
 
-        return [message,flag]
-        
+
+
+        return [message, flag]
+
 
     }
     if (ip > cp) {
-        var flag="bad";
+        var flag = "bad";
         var loss = (ip - cp) * q;
         var lossDecimal = loss.toFixed(2);
         var lossPercentage = ((ip - cp) / ip) * 100;
         var message = "heyy the profit is " + lossDecimal + " and the profit percentage is " + lossPercentage.toFixed(2) + "%";
 
-        return [message,flag]
+        return [message, flag]
 
-    } else {
+    } else if (cp === ip) {
         var flag = "neutral"
         var message = "HODL...........................!"
 
-        return [message,flag]
+        return [message, flag]
 
     }
 
@@ -61,14 +58,21 @@ function profitAndloss(ip, cp, q) {
 
 
 function clickhandler() {
-
     var curr = currentPrice.value;
     var innP = initialPrice.value;
-    var qty  = quantity.value;
-    
-    var [message,status]=profitAndloss(innP,curr,qty);
+    var qty = quantity.value;
 
-    showMessage(message,status)
+    if (curr > 0 && innP > 0 && qty > 0) {
+
+        var [message, status] = profitAndloss(innP, curr, qty);
+        showMessage(message, status)
+
+    }
+    else{
+        outputBox.innerText = "enter all the fields and let them be > 0.";
+    }
+
+
 }
 
-checkButton.addEventListener('click',clickhandler);
+checkButton.addEventListener('click', clickhandler);
