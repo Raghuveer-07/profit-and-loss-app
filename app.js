0f1342c756009771c67593874b5cc4f86fd993a3
Raghuveer-07 <110120267+Raghuveer-07@users.noreply.github.com>
2022-09-05 20:35:33 +0530

    updated : html,css,js

---
+++ app.js
@@ -0,0 +1,74 @@
+const initialPrice = document.querySelector("#inital-price");
+const quantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-price");
+const checkButton = document.querySelector("#check-change");
+const outputBox = document.querySelector("#output-box");
+
+
+function showMessage(msg,status){
+    if (status==="good"){
+        outputBox.style.color = 'green';
+        outputBox.innerText = msg;
+
+    }
+
+    else if (status==="bad"){
+        outputBox.style.color = 'red';
+        outputBox.innerText = msg;
+
+    }
+    else{
+        
+        outputBox.innerText = msg;
+
+    }
+
+}
+
+function profitAndloss(ip, cp, q) {
+    if (ip < cp) {
+        var flag="good";
+        var profit = (cp - ip) * q;
+        var profitDecimal = profit.toFixed(2);
+        var profitPercentage = ((cp - ip) / ip) * 100;
+        var message = "heyy the profit is " + profitDecimal + " and the profit percentage is " + profitPercentage.toFixed(2) + "%" ;
+
+        
+        
+
+        return [message,flag]
+        
+
+    }
+    if (ip > cp) {
+        var flag="bad";
+        var loss = (ip - cp) * q;
+        var lossDecimal = loss.toFixed(2);
+        var lossPercentage = ((ip - cp) / ip) * 100;
+        var message = "heyy the profit is " + lossDecimal + " and the profit percentage is " + lossPercentage.toFixed(2) + "%";
+
+        return [message,flag]
+
+    } else {
+        var flag = "neutral"
+        var message = "HODL...........................!"
+
+        return [message,flag]
+
+    }
+
+}
+
+
+function clickhandler() {
+
+    var curr = currentPrice.value;
+    var innP = initialPrice.value;
+    var qty  = quantity.value;
+    
+    var [message,status]=profitAndloss(innP,curr,qty);
+
+    showMessage(message,status)
+}
+
+checkButton.addEventListener('click',clickhandler);
